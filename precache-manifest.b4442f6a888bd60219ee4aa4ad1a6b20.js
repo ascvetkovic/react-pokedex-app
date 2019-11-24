aa8882f339be44a604a498820b28fb5a7d80fcc6
Aleksandar Sandro Cvetković <sandro.cvetkovic@gmail.com>
2019-11-24 23:38:45 +0100

    Updates

---
+++ precache-manifest.b4442f6a888bd60219ee4aa4ad1a6b20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "705d2d92271c88df967d23ee2994d4ad",
+    "url": "/react-pokedex-app/index.html"
+  },
+  {
+    "revision": "0ea2ba8d3892ea6d8a95",
+    "url": "/react-pokedex-app/static/css/main.a69abf2e.chunk.css"
+  },
+  {
+    "revision": "1c06740ff9e7165d4977",
+    "url": "/react-pokedex-app/static/js/2.4b49d072.chunk.js"
+  },
+  {
+    "revision": "0ea2ba8d3892ea6d8a95",
+    "url": "/react-pokedex-app/static/js/main.d0feaec6.chunk.js"
+  },
+  {
+    "revision": "679fcf67bbdfc7343bd9",
+    "url": "/react-pokedex-app/static/js/runtime-main.cd66e164.js"
+  }
+]);
